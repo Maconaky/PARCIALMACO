bdd710b83e3122c2e9043399aa3f1886d144e19c
FrankChavez <laboratorioesan@WS2302501>
2023-10-15 09:53:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/parcialmaco/CambioActivity.kt
@@ -0,0 +1,11 @@
+package com.example.parcialmaco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CambioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cambio)
+    }
+}
---
+++ app/src/main/java/com/example/parcialmaco/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.parcialmaco
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val etEmail: EditText = findViewById(R.id.etEmail)
+        val etPassword: EditText = findViewById(R.id.etPassword)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if (email == "admin@multiapp.com" && password == "multiapp123") {
+                val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Credenciales inválidas", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+}
